3eb52943389264f8aad8ba7e63b5d86137f74970
Aidana Ualikhan <aidana.ualikhan35@gmail.com>
2022-07-01 12:14:50 +0600

    constructor for App component created

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDispaly';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+    }
+
+    // We have to define render
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (err) => console.log(err)
         );
 
-        return <div>Latintude: </div>;
+        return <div>Latitude: {this.state.lat}</div>;
     }
 }
 
